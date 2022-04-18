a96cad90caea07ce431f543d880a688f7d651d9f
alstjr706 <alstjr706@naver.com>
2022-04-18 13:08:56 +0900

    자바 코드, XML

---
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -0,0 +1,23 @@
+package hello.core.xml;
+
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XmlAppContext {
+
+    @Test
+    void xmlAppContext() {
+        // 그냥 설정 정보만 xml 쪽에서 가져와 사용한것 뿐이다 다른게 아님.
+        // 그 뒤에는 똑같음
+        ApplicationContext ac
+                = new GenericXmlApplicationContext("appConfig.xml");
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberService.class);
+    }
+}
+
+
