4fc965daf001d05ac5d7567ffc320be8af2ac125
alstjr706 <alstjr706@naver.com>
2022-04-27 10:06:30 +0900

    스코프와 프록시 커밋.

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,21 +13,20 @@ import javax.servlet.http.HttpServletRequest;
 @RequiredArgsConstructor
 public class LogDemoController {
 
-
     private final LogDemoService logDemoService;
     private final MyLogger myLogger;
-
 
     @RequestMapping("log-demo") //log-demo 라는 요청이 오면 동작.
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
 
         // 고객이 어떤 URL로 요청했는지 알 수 있다.
-        String requestURL = request.getRequestURI().toString();
+        String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger = " + myLogger.getClass());
 
         // url 정보까지 넣어 주는 것이다.
         myLogger.setRequestURL(requestURL);
-
         myLogger.log("controller test");
         logDemoService.logic("testId");
         return "OK";
